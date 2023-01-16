d3588dfbdcc758cac688cb0f87ec1299c3d57c89
DADAKIM <d.ya96v@gmail.com>
2023-01-16 16:05:44 +0900

    :wrench: 메인페이지 게시글 받아오기

--- hpnyfe/static/js/pages/Home.js
+++ hpnyfe/static/js/pages/Home.js
@@ -1,3 +1,4 @@
+// import { response } from "express";
 import AbstractView from "./AbstractView.js";
 
 export default class extends AbstractView {
@@ -7,22 +8,34 @@ export default class extends AbstractView {
     }
 
     async getHtml() {
+        const response = await fetch("http://43.201.103.199/posts", {
+            method: "GET"
+        })
+            .then((response) => response.json())
+            .then((response) => response.data.posts)
+            .then((response) => response);
+
+            console.log(response)
         return `
             <section class="cont-main">
                 <h2 class="sr-only">메인 페이지</h2>
                 <a href="/newpost" class="btn-new-post" data-link>새 글 작성하기</a>
                 <ul class="mainpage-ul">
-                    <li>
-                        <a href="/postdetail" data-link>
-                            <img class="thumbnail" src="./static/images/test.png" alt="">
-                            <div class="txt-contents">
-                                <h2>신년계획</h2>
-                                <p>모든 국민은 거주·이전의 자유를 가진다. 대통령의 선거에 관한 사항은 법률로 정한다.</p>
-                            </div>
-                        </a>
-                    </li>
+                    ${
+                        response.map((data) => {
+                            return `<li>
+                                <a href="/postdetail" data-link>
+                                    <img class="thumbnail" src=${data.image} alt="">
+                                    <div class="txt-contents">
+                                        <h2>${data.title}</h2>
+                                        <p>${data.content}</p>
+                                    </div>
+                                </a>
+                            </li>`
+                        }).join("")
+                    }
                 </ul>
             </section>
-        `;
+        `
     }
 }
